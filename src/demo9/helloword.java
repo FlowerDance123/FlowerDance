0199d3f1b718d55758b9d1e9d5b45c033c220d11
FlowerDance123 <50074034+FlowerDance123@users.noreply.github.com>
2019-04-28 19:57:30 +0800

    Update helloword.java

--- src/demo9/helloword.java
+++ src/demo9/helloword.java
@@ -5,6 +5,8 @@ public class helloword {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("helloword");
+		System.out.println("helloword");
+		System.out.println("helloword");
 	}
 
 }
