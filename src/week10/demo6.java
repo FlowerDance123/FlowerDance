2e64749025e7007d1fc5c8d83a7cf3a492dc0687
admin <admin@Sjk-Stu042>
2019-05-06 11:39:32 +0800

    week10

---
+++ src/week10/demo6.java
@@ -0,0 +1,18 @@
+package week10;
+
+import java.util.HashSet;
+
+public class demo6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     HashSet h1 = new HashSet();
+     h1.add("kiki");
+	 h1.add("jojo");
+	 h1.add("fofo");
+	 for(Object o:h1) {
+		 System.out.println(o);
+	 }
+	}
+
+}
