5c1eedc3363090fcadf165c7a092b9ba4f5bd34c
984323990 <984323990@qq.com>
2019-05-06 10:48:02 +0800

    week10

--- src/week10/demo2.java
+++ src/week10/demo2.java
@@ -1,6 +1,7 @@
 package week10;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class demo2 {
    
@@ -13,15 +14,18 @@ public class demo2 {
 		a1.add(987);
 		
 		//对所有元素求和
-		int sum = 0;
+//		int sum = 0;
 //		for(int i =0;i <a1.size();i++) {
 //			sum +=(int)a1.get(i);
-		Object[] oj = a1.toArray();
-		for (int i = 0;i < oj.length; i++) {
-			sum +=(int) oj[i];	
-					
-		}
-		System.out.print(sum);
+//		Object[] oj = a1.toArray();
+//		for (int i = 0;i < oj.length; i++) {
+//			sum +=(int) oj[i];	
+	    Iterator iterator = a1.iterator();
+	    while(iterator.hasNext()) {
+	    	System.out.print(iterator.next() + " ");
+	    }
+//		}
+//		System.out.print(sum);
 
 		
 	}
